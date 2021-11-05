db7d24b63882396ce8cf4df9c27511c3c35e7b25
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-05 16:00:06 -0500

    снлф

---
+++ server.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+const express = require('express');
+
+const exphbs = require('express-handlebars');
+
+const routes = require('./controller');
+
+const sequelize = require('./config/connection');
+
+const session = require('express-session');
+
+const withAuth = require('./utils/withAuth');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express()
+const axios = require('axios')
+const hbs = exphbs.create({ });
+
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+const PORT = process.env.PORT || 3001; 
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
